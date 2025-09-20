ca7204524b7cc2396073868f4ee2f2130d06bc1d
sa-hcc5142 <sumaiyaakter0248.gmail.com>
2025-09-21 05:42:48 +0600

    feature branch e change calculator.java

--- calculator.java
+++ calculator.java
@@ -2,6 +2,7 @@ public class calculator{
     public static void main(String[] args) {
         System.out.println("Eta jog : a+b");
         System.out.println("Eta biyog : a-b");
+        System.out.println("Eta feature/calculator branch:");
     }
     
 }
