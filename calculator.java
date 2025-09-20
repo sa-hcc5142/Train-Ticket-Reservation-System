b93adfa07dcf554205860b7b2d6ee14edf591b94
sa-hcc5142 <sumaiyaakter0248.gmail.com>
2025-09-21 05:40:19 +0600

    master branch e change calculator.java

---
+++ calculator.java
@@ -0,0 +1,8 @@
+public class calculator{
+    public static void main(String[] args) {
+        System.out.println("Eta jog : a+b");
+        System.out.println("Eta biyog : a-b");
+        System.out.println("Eta master branch");
+    }
+    
+}
