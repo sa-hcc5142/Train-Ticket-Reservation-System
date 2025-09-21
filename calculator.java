a72328d4e8f8c5a904ccd9d04a03b3779d1549ea
sa-hcc5142 <sumaiyaakter0248.gmail.com>
2025-09-21 06:04:57 +0600

    master branch e merge conflict resolved calculator.java

--- calculator.java
+++ calculator.java
@@ -2,7 +2,6 @@ public class calculator{
     public static void main(String[] args) {
         System.out.println("Eta jog : a+b");
         System.out.println("Eta biyog : a-b");
-        System.out.println("Eta master branch");
     }
     
 }
